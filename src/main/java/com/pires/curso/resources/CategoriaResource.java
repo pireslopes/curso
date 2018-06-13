f7e29064c61cc84ebd3217ab023434bf7645ee75
p_lopes <pireselis@hotmail.com>
2018-06-13 02:20:30 -0300

    inclusão da classe categoria

--- src/main/java/com/pires/curso/resources/CategoriaResource.java
+++ src/main/java/com/pires/curso/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.pires.curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pires.curso.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		return list;
 	}
 }
