57f5d254f7401b4f7efa47d57b2c8a3085ab79da
p_lopes <pireselis@hotmail.com>
2018-06-14 00:19:41 -0300

    Inserindo camadas de serviço e de repositorio

---
+++ src/main/java/com/pires/curso/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.pires.curso.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pires.curso.domain.Categoria;
+import com.pires.curso.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
